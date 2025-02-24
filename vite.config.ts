184c16890f96a450f692090400372b720ffd2748
Ivan Lomovtsev <47413395+advanceddev@users.noreply.github.com>
2025-02-25 00:18:08 +0500

    fix: update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'react-comments-example',
+  base: '/react-comments-example',
   plugins: [react()],
   resolve: {
     alias: {
