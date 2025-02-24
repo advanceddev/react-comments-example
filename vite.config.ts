01b0a29de68382cecfd69578f661e6c288c1562a
Ivan Lomovtsev <47413395+advanceddev@users.noreply.github.com>
2025-02-25 02:27:51 +0500

    feat: add react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/react-comments-example',
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        ["babel-plugin-react-compiler"],
+      ]
+    }
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
